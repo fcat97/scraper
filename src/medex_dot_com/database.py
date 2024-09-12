f570f1c75b1eec4b64b2d2c452724c37dfcb05c4
Shahriar Zaman <nayeem.zxc@gmail.com>
2024-09-13 00:09:32 +0600

    parser updated

--- src/medex_dot_com/database.py
+++ src/medex_dot_com/database.py
@@ -1,5 +1,5 @@
 import sqlite3
-from medex_dot_com.utils import create_dir, db_directory
+from medex_dot_com.utils import create_dir, db_directory, safe_run
 from domain.database import Database
 
 class MedexDatabase(Database):
@@ -47,6 +47,8 @@ class MedexDatabase(Database):
             )
             """)
 
+        self.__do_migration()
+
     def insert_record(self, rec: dict):
         keys = ','.join(rec.keys())
         question_marks = ','.join(list('?'*len(rec)))
@@ -55,6 +57,55 @@ class MedexDatabase(Database):
 
     def save_status(self, url: str, status: str, msg: str|None):
         self.cursor.execute("INSERT INTO status(url, status, msg) VALUES (?, ?, ?)", tuple([url, status, msg]))
+
+    @safe_run
+    def __do_migration(self):
+        self.cursor.execute('PRAGMA user_version')
+        version = self.cursor.fetchone()[0]
+
+        if version == 0:
+            self.__migrate_to_v1()
+            self.cursor.execute('PRAGMA user_version = 1')
+
+    def __migrate_to_v1(self):
+        self.cursor.execute(f"PRAGMA table_info({self.table_name})")
+        columns = [col[1] for col in self.cursor.fetchall()]
+        columns_str = ', '.join(columns)
+
+        self.cursor.execute(f"ALTER TABLE {self.table_name} RENAME TO old_{self.table_name}")
+
+        self.cursor.execute(f"""
+            CREATE TABLE IF NOT EXISTS {self.table_name}(
+                id INTEGER primary key AUTOINCREMENT,
+                url TEXT,
+                name TEXT,
+                type TEXT,
+                generic_group TEXT,
+                manufacturer TEXT,
+                strength TEXT,
+                price TEXT,
+                strip_price TEXT,
+                pack_size_info TEXT,
+                indications TEXT,
+                pharmacology TEXT,
+                dosage TEXT,
+                interaction TEXT,
+                contraindications TEXT,
+                side_effects TEXT,
+                precautions TEXT,
+                pediatric_uses TEXT,
+                pregnancy_cat TEXT,
+                drug_classes TEXT,
+                storage_conditions TEXT,
+                composition TEXT,
+                overdose_effects TEXT,
+                compound_summary TEXT,
+                commonly_asked_questions TEXT
+            )
+        """)
+
+        self.cursor.execute(f'INSERT INTO {self.table_name} ({columns_str}) SELECT {columns_str} FROM old_{self.table_name}')
+        self.cursor.execute(f"DROP TABLE old_{self.table_name}")
 
     def close(self):
         self.connection.commit()
--- src/medex_dot_com/parser.py
+++ src/medex_dot_com/parser.py
@@ -10,16 +10,19 @@ class MedexParser(Parser):
         soup = BeautifulSoup(html, 'html.parser')
         data = dict()
 
-        if not self.is_available(soup):
+        if not self.__is_available(soup):
             return None
 
         data['type'] = self.parse_type(soup)
         data['name'] = self.parse_name(soup)
-        data['generic_group'] = self.parse_group(soup)
+        data['generic_group'] = self.parse_title(soup, "Generic Name")
+        data['manufacturer'] = self.parse_title(soup, "Manufactured by")
+        data['strength'] = self.parse_title(soup, "Strength")
         data['price'] = self.parse_price(soup)
         data['pack_size_info'] = self.parse_pack_size(soup)
         data['strip_price'] = self.parse_strip_price(soup)
         data['indications'] = self.parse_section(soup, 'indications')
+        data['composition'] = self.parse_section(soup, 'composition')
         data['pharmacology'] = self.parse_section(soup, 'mode_of_action')
         data['dosage'] = self.parse_section(soup, 'dosage')
         data['interaction'] = self.parse_section(soup, 'interaction')
@@ -30,10 +33,13 @@ class MedexParser(Parser):
         data['pediatric_uses'] = self.parse_section(soup, 'pediatric_uses')
         data['drug_classes'] = self.parse_section(soup, 'drug_classes')
         data['storage_conditions'] = self.parse_section(soup, 'storage_conditions')
+        data['overdose_effects'] = self.parse_section(soup, 'overdose_effects')
+        data['compound_summary'] = self.parse_section(soup, 'compound_summary', True)
+        data['commonly_asked_questions'] = self.parse_section(soup, 'commonly_asked_questions', True)
 
         return data
 
-    def is_available(self, soup: BeautifulSoup) -> bool:
+    def __is_available(self, soup: BeautifulSoup) -> bool:
         code = soup.find('div', class_='code')
         if code:
             if code.text == '404':
@@ -56,23 +62,26 @@ class MedexParser(Parser):
         return h1_tag.text.strip()
 
     @safe_run
-    def parse_type(self, soup: BeautifulSoup) -> str:
+    def parse_title(self, soup, title: str) -> str|None:
+        div = soup.find('div', title=title)
+        if div:
+            return div.text.strip()
+        else:
+            return None
+
+    @safe_run
+    def parse_type(self, soup: BeautifulSoup) -> str|None:
         small_tag = soup.find('small', class_='h1-subtitle')
-        return small_tag.text.strip()
+        if small_tag:
+            return small_tag.text.strip()
+        else:
+            return None
 
     @safe_run
-    def parse_group(self, soup: BeautifulSoup) -> str:
-        return soup.find('div', title='Generic Name').text.strip()
-
-    @safe_run
-    def parse_manufacturer(self, soup: BeautifulSoup) -> str:
-        return soup.find('div', title='Manufactured by').text.strip()
-
-    @safe_run
-    def parse_price(self, soup: BeautifulSoup) -> str:
+    def parse_price(self, soup: BeautifulSoup) -> str|None:
         unit_price_span = soup.select_one('.package-container > span:nth-of-type(2)')
         if unit_price_span is None:
-            return ""
+            return None
         else:
             return unit_price_span.text.strip()
 
@@ -81,13 +90,20 @@ class MedexParser(Parser):
         return soup.find('span', class_='pack-size-info').text.strip()
 
     @safe_run
-    def parse_strip_price(self, soup: BeautifulSoup) -> str:
+    def parse_strip_price(self, soup: BeautifulSoup) -> str|None:
         strip_price_span = soup.select_one('.package-container div > span:nth-of-type(2)')
         if strip_price_span is None:
-            return ""
+            return None
         else:
             return strip_price_span.text.strip()
 
     @safe_run
-    def parse_section(self, soup: BeautifulSoup, section: str) -> str:
-        return soup.find(id=section).find_next_sibling().text.strip()
+    def parse_section(self, soup: BeautifulSoup, section: str, as_html: bool = False) -> str|None:
+        span = soup.find(id=section)
+        if not span:
+            return None
+
+        if as_html:
+            return span.find_next_sibling().contents
+        else:
+            return span.find_next_sibling().text.strip()
